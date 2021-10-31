702e79cf2742f2f24e5e40d8c7f48a31a2b7fbd4
Jones Agyemang <jones.agyemang@thoughtworks.com>
2021-10-31 23:52:26 +0000

    returns list of ordered webpages with most views

---
+++ spec/error_handling_spec.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+require_relative '../lib/errors/missing_logfile_error'
+
+RSpec.describe 'error handling' do
+  context 'missing log file' do
+    it 'throws an exception' do
+      expect do
+        system('./lib/parser.rb')
+      end.to output(/Missing log file/).to_stderr_from_any_process
+    end
+  end
+
+  context 'log file is available' do
+    it 'should not throw an exception' do
+      expect do
+        system('./lib/parser.rb anyfile.log')
+      end.to_not output(/Missing log file/).to_stderr_from_any_process
+    end
+  end
+
+  context 'log file does not exist' do
+    it 'should raise an exception' do
+      expect do
+        system('./lib/parser.rb nonexistent.log')
+      end.to output(/Log file not found/).to_stderr_from_any_process
+    end
+  end
+
+  context 'log file exists' do
+    it 'should not raise an exception' do
+      expect do
+        system('./lib/parser.rb webserver.log')
+      end.not_to output(/Log file not found/).to_stderr_from_any_process
+    end
+  end
+end
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -1,37 +1,11 @@
 # frozen_string_literal: true
 
-require_relative '../lib/errors/missing_logfile_error'
+RSpec.describe 'parser' do
+  it 'returns ordered list of webpages with most views' do
+    expected_result = '/home 3 visits /about/2 2 visits /help_page/1 1 visits '
 
-RSpec.describe 'error handling' do
-  context 'missing log file' do
-    it 'throws an exception' do
-      expect do
-        system('./lib/parser.rb')
-      end.to output(/Missing log file/).to_stderr_from_any_process
-    end
-  end
-
-  context 'log file is available' do
-    it 'should not throw an exception' do
-      expect do
-        system('./lib/parser.rb anyfile.log')
-      end.to_not output(/Missing log file/).to_stderr_from_any_process
-    end
-  end
-
-  context 'log file does not exist' do
-    it 'should raise an exception' do
-      expect do
-        system('./lib/parser.rb nonexistent.log')
-      end.to output(/Log file not found/).to_stderr_from_any_process
-    end
-  end
-
-  context 'log file exists' do
-    it 'should not raise an exception' do
-      expect do
-        system('./lib/parser.rb webserver.log')
-      end.not_to output(/Log file not found/).to_stderr_from_any_process
-    end
+    expect do
+      system('./lib/parser.rb sample.log')
+    end.to output(expected_result).to_stdout_from_any_process
   end
 end
