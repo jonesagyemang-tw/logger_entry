4db6e6cefc0734d29e865583e686d37a6c1078e1
Jones Agyemang <jones.agyemang@thoughtworks.com>
2021-10-31 20:40:34 +0000

    Adds case for when file name is provided

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -10,4 +10,12 @@ RSpec.describe 'error handling' do
       end.to output(/Missing log file/).to_stderr_from_any_process
     end
   end
+
+  context 'log file is available' do
+    it 'should not throw an exception' do
+      expect do
+        system('./lib/parser.rb anyfile.log')
+      end.to_not output(/Missing log file/).to_stderr_from_any_process
+    end
+  end
 end
