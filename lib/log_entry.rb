621785c0070585ea1641fd317c02a012307d655d
Jones Agyemang <jones.agyemang@thoughtworks.com>
2021-11-02 20:21:19 +0000

    Moves regex to const

--- lib/log_entry.rb
+++ lib/log_entry.rb
@@ -5,12 +5,14 @@
 class LogEntry
   attr_reader :uri, :ip_address
 
+  IP_REGEX = /^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/
+
   def initialize(uri:, ip_address:)
     @uri = uri
     @ip_address = ip_address
   end
 
   def valid?
-    !uri.nil? && !uri.empty? && /^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/.match?(ip_address)
+    !uri.nil? && !uri.empty? && IP_REGEX.match?(ip_address)
   end
 end
