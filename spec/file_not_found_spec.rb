4046c0ec894165dae9da4cb48c4418329cb7a282
Jones Agyemang <jones.agyemang@thoughtworks.com>
2021-11-03 10:17:59 +0000

    Add note on improvement to be made on log file spec

--- spec/file_not_found_spec.rb
+++ spec/file_not_found_spec.rb
@@ -3,6 +3,7 @@
 RSpec.describe 'error handling' do
   context 'log file does not exist' do
     it 'should raise an exception' do
+      # FIX ME: Replace with unit tests. Failure should be triggered from calling a Parser object
       expect do
         system('./lib/main.rb nonexistent.log')
       end.to output(/Log file not found/).to_stderr_from_any_process
@@ -11,6 +12,7 @@ RSpec.describe 'error handling' do
 
   context 'log file exists' do
     it 'should not raise an exception' do
+      # FIX ME: Replace with unit tests. Failure should be triggered from calling a Parser object
       expect do
         system('./lib/main.rb sample.log')
       end.not_to output(/Log file not found/).to_stderr_from_any_process
--- spec/missing_log_file_spec.rb
+++ spec/missing_log_file_spec.rb
@@ -3,6 +3,7 @@
 RSpec.describe 'error handling' do
   context 'missing log file' do
     it 'throws an exception' do
+      # FIX ME: Replace with unit tests. Failure should be triggered from calling a Parser object
       expect do
         system('./lib/main.rb')
       end.to output(/Missing log file/).to_stderr_from_any_process
@@ -11,6 +12,7 @@ RSpec.describe 'error handling' do
 
   context 'log file is available' do
     it 'should not throw an exception' do
+      # FIX ME: Replace with unit tests. Failure should be triggered from calling a Parser object
       expect do
         system('./lib/main.rb anyfile.log')
       end.to_not output(/Missing log file/).to_stderr_from_any_process
